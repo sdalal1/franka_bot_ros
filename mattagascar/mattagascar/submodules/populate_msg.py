5a48fa46507144f5ee283718abd98f5b97f58b2e
dbarsoum <demianabarsoum2027@u.northwestern.edu>
2023-11-11 12:45:11 -0600

    notes in pop msgs about importance of setting vel constrainsts

--- mattagascar/mattagascar/submodules/populate_msg.py
+++ mattagascar/mattagascar/submodules/populate_msg.py
@@ -85,12 +85,13 @@ class PopulateMsgs():
         cart_pos_rqst.link_name = 'panda_link8'
         cart_pos_rqst.waypoints = waypoints  # this must be a list of Pose() msgs
         max_step = 0.1
+        # not setting these will cause the motion to be fastttttt!
         cart_pos_rqst.max_velocity_scaling_factor = 0.25
         cart_pos_rqst.max_acceleration_scaling_factor = 0.25
         cart_pos_rqst.max_cartesian_speed = 0.15
         cart_pos_rqst.cartesian_speed_limited_link = 'panda_link8'
         cart_pos_rqst.max_step = max_step
-        # cart_pos_rqst.jump_threshold = jump_threshold 
+        # cart_pos_rqst.jump_threshold = jump_threshold
         return cart_pos_rqst
 
     
