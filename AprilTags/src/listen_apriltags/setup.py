a6055c2f6f93c15d4f43af00beee8bfd166b8e91
sdalal <shaildalal2024@u.northwestern.edu>
2023-11-15 20:15:57 -0600

    Apriltags imported to cart

---
+++ AprilTags/src/listen_apriltags/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'listen_apriltags'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml', 'launch/aprilTags.launch.xml', 'config/tags.yaml', 'config/rviz_config.rviz']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='csmith',
+    maintainer_email='courtney77smith77@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'listener = listen_apriltags.listening:main'
+        ],
+    },
+)
